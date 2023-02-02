a551f849e4976ec742107a6c5657a6e8cf9bec25
annafu1 <104175541+annafu1@users.noreply.github.com>
2023-02-02 17:44:59 -0500

    added in asc order file

---
+++ answers/AscendingOrder.sql
@@ -0,0 +1,4 @@
+--A mailing list of an illegal online service was sent to the SQLPD.
+--Submit all entries details sorted by family name in asc.
+SELECT * FROM mailing_list
+ORDER BY familyName ASC;
